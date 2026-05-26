938f37ceb9bc33b6a8b48d2cbcdf77844799fba8
christine <christineding0211@gmail.com>
2026-05-26 18:03:12 +0800

    revised mysqloperator style

---
+++ dataflow/etl/refresh_stg_pchome.sql
@@ -0,0 +1,53 @@
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+
+sql_script = """
+-- if scraped failed, delete that day data to ensure no duplicated 
+DELETE FROM stg_pchome_prices 
+WHERE scraped_date = '{{ ds }}';
+
+INSERT INTO stg_pchome_prices  (
+    ID, 
+    scraped_date, 
+    Name, 
+    Price, 
+    Rating,  
+    Source, 
+    scraped_at
+)
+WITH deduplicated AS (
+    SELECT 
+        ID, 
+        DATE(scraped_at) AS scraped_date, 
+        Name, 
+        Price, 
+        Rating, 
+        Source, 
+        scraped_at,
+        ROW_NUMBER() OVER(
+            PARTITION BY ID, DATE(scraped_at) 
+            ORDER BY scraped_at ASC
+        ) AS row_num
+    FROM raw_pchome_prices
+    WHERE DATE(scraped_at) = '{{ ds }}' 
+)
+
+SELECT 
+    ID, 
+    scraped_date, 
+    Name, 
+    Price, 
+    Rating, 
+    Source, 
+    scraped_at
+FROM deduplicated
+WHERE row_num = 1;
+"""
+
+def create_refresh_stg_pchome_task():
+
+    return MySqlOperator(
+        task_id="refresh_stg_pchome",
+        mysql_conn_id="mysql_default",
+        sql=sql_script
+    )
+    
