521c8c80d26fe936544d3e911a040f3b9bc6d032
christine <christineding0211@gmail.com>
2026-05-29 12:24:59 +0800

    add condition (福利品)

--- dataflow/etl/sql/refresh_fct_daily.sql
+++ dataflow/etl/sql/refresh_fct_daily.sql
@@ -12,8 +12,11 @@ FROM (
         ROW_NUMBER() OVER(PARTITION BY momo.ID, momo.scraped_date ORDER BY LENGTH(dim.keyword) DESC) AS rank_id
     FROM stg_momo_prices momo
     JOIN dim_products dim ON LOWER(momo.Name) LIKE CONCAT('%', dim.keyword, '%')
-    
     WHERE momo.scraped_date = '{{ ds }}'
+      -- 排除 momo 的福利品與整新品
+      AND momo.Name NOT LIKE '%福利品%'
+      AND momo.Name NOT LIKE '%整新品%'
+      AND momo.Name NOT LIKE '%展示機%'
 
     UNION ALL
 
@@ -22,7 +25,10 @@ FROM (
         ROW_NUMBER() OVER(PARTITION BY pch.ID, pch.scraped_date ORDER BY LENGTH(dim.keyword) DESC) AS rank_id
     FROM stg_pchome_prices pch
     JOIN dim_products dim ON LOWER(pch.Name) LIKE CONCAT('%', dim.keyword, '%')
-    
     WHERE pch.scraped_date = '{{ ds }}'
+      -- 排除 PChome 的福利品與整新品
+      AND pch.Name NOT LIKE '%福利品%'
+      AND pch.Name NOT LIKE '%整新品%'
+      AND pch.Name NOT LIKE '%拆封%'
 ) combined_platforms
 WHERE rank_id = 1;
