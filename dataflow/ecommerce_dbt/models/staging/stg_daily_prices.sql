84a4bd5cb8cc19c4bfe1ad39a2236f2d942c2b61
christine <christineding0211@gmail.com>
2026-07-27 15:51:31 +0800

    add stg_daily_prices for cleaned data

--- dataflow/ecommerce_dbt/models/staging/stg_daily_prices.sql
+++ dataflow/ecommerce_dbt/models/staging/stg_daily_prices.sql
@@ -1,27 +1,41 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-{{ config(materialized='table') }}
-
+-- cleaning fct_daily_prices
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+    select
+        scraped_date,
+        brand,
+        official_model_name,
+        platform,
+        original_code,
+        price
+    from {{ source('ecommerce', 'fct_daily_prices') }}
+),
+
+cleaned_data as (
+
+    select
+        cast(scraped_date as date) as scraped_date,
+        trim(brand) as brand,
+        trim(official_model_name) as official_model_name,
+        lower(trim(platform)) as platform,
+        cast(original_code as string) as original_code,
+        safe_cast(price as numeric) as price
+    from source_data
 
 )
 
-select *
-from source_data
+select
+    scraped_date,
+    brand,
+    official_model_name,
+    platform,
+    original_code,
+    price
+from cleaned_data
+where scraped_date is not null
+  and brand is not null
+  and official_model_name is not null
+  and platform in ('momo', 'pchome')
+  and price is not null
+  and price > 0
 
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
