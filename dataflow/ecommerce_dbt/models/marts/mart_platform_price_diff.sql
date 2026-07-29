852e09bd269103fa7c119154ca80fc0ca6c4b3ee
christine <christineding0211@gmail.com>
2026-07-29 14:00:38 +0800

    change CTE name differ from column name

--- dataflow/ecommerce_dbt/models/marts/mart_platform_price_diff.sql
+++ dataflow/ecommerce_dbt/models/marts/mart_platform_price_diff.sql
@@ -7,23 +7,29 @@ WITH bundled_prices AS (
         MAX(CASE WHEN platform = 'pchome' THEN min_price END) AS pchome_price
     FROM {{ ref('int_daily_best_deals') }}
     GROUP BY scraped_date, brand, official_model_name
-),
-price_diff AS (
+), 
+calculated_price_diff as (
 
-    SELECT
+    select
         scraped_date,
         brand,
         official_model_name,
         momo_price,
         pchome_price,
-        momo_price - pchome_price AS price_diff
-    FROM bundled_prices
+        momo_price - pchome_price as price_diff
+    from bundled_prices
+
 )
 -- 使用 SAFE_DIVIDE ，預防 PChome 缺貨變 0 導致管線崩潰
-SELECT
-    *,
-    ROUND(
-        SAFE_DIVIDE(price_diff, pchome_price) * 100,
+select
+    scraped_date,
+    brand,
+    official_model_name,
+    momo_price,
+    pchome_price,
+    price_diff,
+    round(
+        safe_divide(price_diff, pchome_price) * 100,
         2
-    ) AS price_diff_pct
-FROM price_diff
+    ) as price_diff_pct
+from calculated_price_diff
